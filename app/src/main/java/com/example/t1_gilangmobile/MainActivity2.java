793ddc4a91fd81f743bff874718654f11e2c05ad
gilangmaulidi4517 <gilangmaulidi4517@gmail.com>
2025-03-17 19:27:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/t1_gilangmobile/MainActivity2.java
@@ -0,0 +1,61 @@
+package com.example.t1_gilangmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),(v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        //
+         //
+
+        //
+
+
+        findViewById(R.id.btnDaftar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+            }
+        });
+
+
+        findViewById(R.id.btnMasuk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+            }
+        });
+
+
+
+
+
+
+
+
+        //
+
+    }
+}
